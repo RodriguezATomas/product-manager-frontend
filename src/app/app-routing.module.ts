afd3979dd8d2ce17e761495edc2137f25e9455ce
tomas <164423065+RodriguezATomas@users.noreply.github.com>
2026-04-20 00:03:39 -0300

    #feat: new css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }, { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) }, { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'users' },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) },
+  { path: '**', redirectTo: 'users' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
